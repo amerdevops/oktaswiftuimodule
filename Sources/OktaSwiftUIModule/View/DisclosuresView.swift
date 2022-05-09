2dbe266f12bdae433cf4ebadc37987906a16a945
Sandeep Madineni <sandeep.madineni@ameritas.com>
2022-05-09 19:54:52 -0400

    Adding disclosure

---
+++ Sources/OktaSwiftUIModule/View/DisclosuresView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  
+//
+//  Created by Sandeep Madineni on 5/8/22.
+//
+
+import SwiftUI
+
+struct DisclosuresView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        DisclosuresView()
+    }
+}
