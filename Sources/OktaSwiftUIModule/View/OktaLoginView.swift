0867b60f2ef0229a4d83902ec1624cbf7893631c
Sandeep Madineni <sandeep.madineni@ameritas.com>
2022-05-12 16:54:21 -0400

    adding fonts

--- Sources/OktaSwiftUIModule/View/OktaLoginView.swift
+++ Sources/OktaSwiftUIModule/View/OktaLoginView.swift
@@ -85,8 +85,10 @@ public struct OktaLoginView: View {
                 
                 HStack{
                     Text("By Signing in, you agree to the [Ameritas Online Privacy Notice](https://www.ameritas.com/about/online-privacy/) and [Legal/Terms of Use](https://www.ameritas.com/about/legal-terms-of-use)")
-                    
+                        .font(K.BrandFont.regular16)
+                        .foregroundColor(K.BrandColor.lightDarkGray)
                 }
+                .padding(EdgeInsets(top: 42, leading: 16, bottom: 0, trailing: 16))
                 .frame(maxWidth: .infinity, minHeight: 44)
                 
                 //-----------------------------------------------
